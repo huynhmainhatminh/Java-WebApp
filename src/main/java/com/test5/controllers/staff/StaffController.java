d07addd311d4ad8c8b456dc54e0065f29664d761
Huỳnh Mai Nhật Minh <156114356+huynhmainhatminh@users.noreply.github.com>
2025-10-20 12:49:45 +0700

    cập nhật staff controller, hiển thị giao diện đăng nhập

--- src/main/java/com/test5/controllers/staff/StaffController.java
+++ src/main/java/com/test5/controllers/staff/StaffController.java
@@ -1,5 +1,16 @@
 package com.test5.controllers.staff;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/staff")
 public class StaffController {
 
+    @GetMapping("/login")
+    public String staffArea() {
+        return "/staff/login";
+    }
 }
