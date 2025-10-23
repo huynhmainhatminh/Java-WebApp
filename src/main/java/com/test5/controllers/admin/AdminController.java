01f87c5822cc44fba495a98e8e999a9e0eb752b1
NguyenHaNam657 <nguyentannho220992@gmail.com>
2025-10-23 23:52:47 +0700

    Cập nhật các route cho Admin

--- src/main/java/com/test5/controllers/admin/AdminController.java
+++ src/main/java/com/test5/controllers/admin/AdminController.java
@@ -13,4 +13,26 @@ public class AdminController {
     public String adminArea() {
         return "/admin/login";
     }
+
+    @GetMapping("/manage-member")
+    public String manage_member() {
+        return "/admin/manage-member";
+    }
+
+
+    @GetMapping("/manage-staff")
+    public String manage_staff() {
+        return "/admin/manage-staff";
+    }
+
+    @GetMapping("/manage-admin")
+    public String manage_admin() {
+        return "/admin/manage-admin";
+    }
+
+    @GetMapping("/dashboard")
+    public String myArea() {
+        return "/admin/dashboard";
+    }
+
 }
