4b548f1d7bc27aa26725a049737f7e08babeccc2
Mun <taikhoanao3663@gmail.com>
2025-10-27 21:58:32 +0700

    cap nhat respone cho API dang ky dich vu

--- src/main/java/com/restapi/api/controllers/DRIVER/DriverController.java
+++ src/main/java/com/restapi/api/controllers/DRIVER/DriverController.java
@@ -1,10 +1,12 @@
 package com.restapi.api.controllers.DRIVER;
 import com.restapi.api.model.requests.RegisterPackageRequests;
+import com.restapi.api.model.response.ResponseAPI;
 import com.restapi.api.pojo.RentalPackage;
 import com.restapi.api.pojo.User;
 import com.restapi.api.services.GetInformationServices;
 import com.restapi.api.services.RegisterPackageServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -19,16 +21,25 @@ public class DriverController {
     GetInformationServices getInformation;
 
     @PostMapping("/registerPackage")
-    public String registerPack(@ModelAttribute RegisterPackageRequests registerPackageRequests){
-        RentalPackage rentalPackage = new RentalPackage();
-        User user = new User();
-        user.setId(registerPackageRequests.getUserId());
-        rentalPackage.setUser(user);
-        rentalPackage.setName(registerPackageRequests.getName());
-        rentalPackage.setPrice(registerPackageRequests.getPrice());
-        rentalPackage.setDurationDays(registerPackageRequests.getDuration_days());
-        registerPackageServices.registerPack(rentalPackage);
-        return "Đăng ký dịch vụ thành công";
+    public ResponseEntity<?> registerPack(@ModelAttribute RegisterPackageRequests registerPackageRequests){
+        try{
+            RentalPackage rentalPackage = new RentalPackage();
+            User user = new User();
+            user.setId(registerPackageRequests.getUserId());
+            rentalPackage.setUser(user);
+            rentalPackage.setName(registerPackageRequests.getName());
+            rentalPackage.setPrice(registerPackageRequests.getPrice());
+            rentalPackage.setDurationDays(registerPackageRequests.getDuration_days());
+            registerPackageServices.registerPack(rentalPackage);
+            return ResponseEntity.ok(
+                    new ResponseAPI(true, "Đăng ký dịch vụ thành công")
+            );
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(
+                    new ResponseAPI(false, "Đăng ký dịch vụ thất bại")
+            );
+        }
+
     }
 
 
