38f3867143f18d9fd149355c36f5bf51e6edb771
Huỳnh Mai Nhật Minh <156114356+huynhmainhatminh@users.noreply.github.com>
2025-11-10 19:44:30 +0700

    xây dựng lớp Authorization cơ bản

---
+++ src/main/java/com/ev/batteryswap/security/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.ev.batteryswap.security;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity // Bật module bảo mật Spring Security
+public class SecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .formLogin(AbstractHttpConfigurer::disable); // vô hiệu hóa form login mặc định của Spring Security
+
+        return http.build();
+    }
+
+}
