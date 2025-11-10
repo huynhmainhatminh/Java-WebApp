27bf433eb9013acc1943d6b96ed18db61b9ed211
Huỳnh Mai Nhật Minh <156114356+huynhmainhatminh@users.noreply.github.com>
2025-11-11 02:07:15 +0700

    xây dựng lớp Login kết hợp với set cookie người dùng bằng token jwt

--- src/main/java/com/ev/batteryswap/controllers/AuthController.java
+++ src/main/java/com/ev/batteryswap/controllers/AuthController.java
@@ -2,8 +2,13 @@ package com.ev.batteryswap.controllers;
 import com.ev.batteryswap.dto.AuthRequestLogin;
 import com.ev.batteryswap.dto.AuthRequestRegister;
 import com.ev.batteryswap.pojo.User;
+import com.ev.batteryswap.security.JwtUtils;
 import com.ev.batteryswap.services.AuthServices;
+import com.ev.batteryswap.services.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,20 +20,40 @@ public class AuthController {
     @Autowired
     AuthServices authServices;
 
+    @Autowired
+    UserService userService;
 
+    @Autowired
+    JwtUtils jwtUtils;
 
-
+    // API đăng nhập
     @PostMapping("/login")
-    public ResponseEntity<?> login(@ModelAttribute AuthRequestLogin authRequest) {
-        if (authServices.login(authRequest.getUsername(), authRequest.getPassword())) {
+    public ResponseEntity<?> login(@ModelAttribute AuthRequestLogin authRequestLogin, HttpServletResponse response) {
+
+        // kiểm tra username và password có tồn tại trong database
+        if (authServices.login(authRequestLogin.getUsername(), authRequestLogin.getPassword())) {
+
+            // nếu True
+            String role = userService.findByUsername(authRequestLogin.getUsername()).getRole(); // lấy role người dùng
+
+            String token = jwtUtils.generateToken(authRequestLogin.getUsername(), role); // tạo token
+
+            ResponseCookie cookie = ResponseCookie.from("jwt", token)
+                    .httpOnly(true) // ẩn với JS
+                    .secure(false) // đặt true nếu dùng HTTPS
+                    .path("/") // phạm vi đường dẫn, "/" nghĩa là gửi cho mọi endpoint trên domain.
+                    .maxAge(3600)  // thời hạn (giây), Hết hạn → trình duyệt tự xóa cookie.
+                    .build(); // tạo đối tượng và đính vào response
+            response.addHeader("Set-Cookie", cookie.toString());
+
             return ResponseEntity.ok("Đăng nhập thành công.");
         } else {
             return ResponseEntity.badRequest().body("Đăng nhập thất bại.");
         }
-        // return authServices.login(authRequest.getUsername(), authRequest.getPassword());
     }
 
 
+    // API đăng ký
     @PostMapping("/register")
     public User register(@RequestBody AuthRequestRegister authRequest) {
         if (!authRequest.getPassword().equals(authRequest.getConfirmPassword())) {
