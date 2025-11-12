f23752c1d73a340bd1b438425dc36c1866ee9eee
9hNek <bulletz6105@gmail.com>
2025-11-12 19:18:08 +0700

    Thêm phân quyền truy cập trang theo từng role

--- src/main/java/com/ev/batteryswap/security/SecurityConfig.java
+++ src/main/java/com/ev/batteryswap/security/SecurityConfig.java
@@ -9,7 +9,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableWebSecurity // Bật module bảo mật Spring Security
+@EnableWebSecurity
 public class SecurityConfig {
 
     private final JwtAuthFilter jwtAuthFilter;
@@ -24,15 +24,28 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/login", "/register", "/my","/packages","/admin/login","/staff/login","/api/**").permitAll() // API công khai
-                        .requestMatchers("/css/**", "/js/**", "/images/**", "/img/**").permitAll() // hiệu ứng
+                        // trang công khai
+                        .requestMatchers(
+                                "/", "/login", "/register", "/packages",
+                                "/admin/login", "/staff/login", // Cho phép 2 trang login
+                                "/api/**", // Cho phép mọi API
+                                "/css/**", "/js/**", "/img/**" // Cho phép file tĩnh
+                        ).permitAll()
+
+                        //trang cho driver
+                        .requestMatchers("/my", "/naptien", "/contact", "/dashboard").hasAnyAuthority("DRIVER")
+
+                        //trang cho staff
+                        .requestMatchers("/staff/**").hasAnyAuthority("STAFF")
+
+                        //trang cho admin
                         .requestMatchers("/admin/**").hasAnyAuthority("ADMIN")
+
                         .anyRequest().authenticated()
                 )
-                .formLogin(AbstractHttpConfigurer::disable) // vô hiệu hóa form login mặc định của Spring Security
+                .formLogin(AbstractHttpConfigurer::disable)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
-
 }
